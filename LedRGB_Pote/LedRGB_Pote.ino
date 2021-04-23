dcbe554b4c29f26dcd0af05d8b60fa6a3ba45301
Ivan Fardjoume <ivan.fardjoume@gmail.com>
2021-04-23 13:19:48 -0300

    Agrega codigo del LedRGB+Pote

---
+++ LedRGB_Pote/LedRGB_Pote.ino
@@ -0,0 +1,24 @@
+#define LED_RGB_R 5 // D1
+#define LED_RGB_G 4 // D2
+#define LED_RGB_B 0 // D3
+
+
+
+void setup() {
+  pinMode(LED_RGB_R, OUTPUT);
+  pinMode(LED_RGB_G, OUTPUT);
+  pinMode(LED_RGB_B, OUTPUT);
+}
+
+void loop() {
+  int PoteValue = analogRead(A0);
+  if (PoteValue <= 341) {
+    digitalWrite(LED_RGB_R, LOW);
+  }
+  if (PoteValue > 341 && PoteValue <= 682) {
+    digitalWrite(LED_RGB_G, LOW);
+  }
+  if (PoteValue > 682) {
+    digitalWrite(LED_RGB_B, LOW);
+  }
+}
